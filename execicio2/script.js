f67dcc586236cf1d9c6c6d70452eb322d97af4ec
Adao H <adaoaraujo123@gmail.com>
2022-05-05 15:49:01 -0300

    Exercício 2.1

--- execicio2/script.js
+++ execicio2/script.js
@@ -1,3 +1,6 @@
+/*Crie um botão em um ficheiro html, que ao ser clicado aparece
+um número aleatório entre 0 e 50
+*/
 const enviar = document.querySelector('.enviar');
    var h2    = document.querySelector('.texto' );
 enviar.addEventListener('click', function(event){
@@ -10,3 +13,14 @@ function gerarNumeroaletorio(){
         const min = 0;
         return Math.floor(Math.random() * (max - min)) + min;   
 }
+/**
+ * Exercício 2.1
+Crie um botão em um ficheiro html, que ao ser clicado abre
+uma janela nova com o site: http://www.imdb.com
+ */
+
+const enviarr = document.querySelector('.enviarr');
+   var h2    = document.querySelector('.texto' );
+enviarr.addEventListener('click', function(event){
+    window.open("http://www.imdb.com","_blank");
+});
